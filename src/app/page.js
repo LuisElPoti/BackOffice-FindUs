1206f6793fe3568398d809fb94482ef18a5e9a38
Cristian Jimenez <1106166@est.intec.edu.do>
2024-09-09 10:15:23 -0400

    cambios pantalla home y users frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,29 @@
 "use client";
-import SideBar from "./components/sidebar";
+import React, {useState} from "react";
+import Sidebar from "./components/sidebar";
+import Home from "./home/page";
+import Users from "./users/page";
+import Publicaciones from "./publicaciones/page";
+import Material from "./material/page";
+import Servicios from "./servicios/page";
 
 export default function Page() {
-  return <SideBar></SideBar>
+
+  const [selectedScreen, setSelectedScreen] = useState('home');
+
+  return (
+    <div className="flex">
+      <Sidebar setSelectedScreen={setSelectedScreen}></Sidebar>
+
+      <div className= "flex-1">
+      {selectedScreen === 'home' && <Home/>}
+      {selectedScreen === 'users' && <Users/>}
+      {selectedScreen === 'publicaciones' && <Publicaciones/>}
+      {selectedScreen === 'material' && <Material/>}
+      {selectedScreen === 'servicios' && <Servicios/>}
+      </div>
+    </div>
+
+    
+  );
 }
